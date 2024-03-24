5bd342103fe5d86b4fb63f43adbbcc35acee41ee
HXHdevs <jordisonnicolas@gmail.com>
2024-03-24 17:51:20 -0600

    Rewriting all files from /views

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,80 +1,88 @@
-// Con esto evitas el flasheo al hacer reload de site en site
-import { createRef, useState } from 'react';
+import { createRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Alerta from '../components/Alerta';
 import { useAuth } from '../hooks/useAuth';
 
 export default function Login() {
-  
-  const emailRef = createRef();
-  const passwordRef = createRef();
+    
+    const emailRef = createRef();
+    const passwordRef = createRef();
 
-  const [errores, setErrores] = useState([])
-  const {login} = useAuth({
-    middleware: 'guest',
-    url: '/'
-  })
+    const [errores, setErrores] = useState([])
+    const { login } = useAuth({
+        middleware: 'guest',
+        url: '/'
+    })
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+    const handleSubmit = async e => {
+        e.preventDefault();
 
-    const datos = {
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
+        const datos = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+       
+        login(datos, setErrores)
     }
-    login(datos, setErrores)
-  }
-  return (
-    <>
-        <h1 className="text-4xl font-black">Iniciar sesion</h1>
-        <p>Inicia sesion para poder hacer tus pedidos</p>
+
+
+    return (
+        <>
+            <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+            <p>Para crear un pedido debes iniciar sesión</p>
+
+            <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+                <form
+                    onSubmit={handleSubmit}
+                    noValidate
+                >
+
+                    {errores ? errores.map((error, i) => <Alerta key={i}>{error}</Alerta>)  : null }
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="email"
+                        >Email:</label>
+                        <input 
+                            type="email" 
+                            id="email"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="email"
+                            placeholder="Tu Email"
+                            ref={emailRef}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="password"
+                        >Password:</label>
+                        <input 
+                            type="password" 
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="password"
+                            placeholder="Tu Password"
+                            ref={passwordRef}
+                        />
+                    </div>
+
         
-        <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-            <form
-              onSubmit={handleSubmit}
-              noValidate
-            >
-                 {errores ? errores.map((error, i) => <Alerta key={i}>{error}</Alerta>) : null}
-                 <div className="mb-4">
-                    <label 
-                    className="text-slate-800"
-                    htmlFor="correo"
-                    >Correo:</label>
                     <input
-                        type="text"
-                        id="correo"
-                        className="mt-2 w-full p-2 bg-gray-50"
-                        name="correo"
-                        placeholder="Tu correo"
-                        ref={emailRef}
+                        type="submit"
+                        value="Iniciar Sesión"
+                        className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
                     />
-                 </div>
-                 <div className="mb-4">
-                    <label 
-                    className="text-slate-800"
-                    htmlFor="name"
-                    >Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="mt-2 w-full p-2 bg-gray-50"
-                        name="password"
-                        placeholder="Tu password"
-                        ref={passwordRef}
-                    />
-                 </div>
-                 <input
-                  type="submit"
-                  value="Iniciar sesion"
-                  className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
-                  />
-            </form>
-        </div>
-        <nav className="mt-5">
-          <Link to="/auth/registro">
-            No tienes cuenta? Crea Una
-          </Link>
-        </nav>
-      </>
-  )
+                </form>
+            </div>
+
+            <nav className="mt-5">
+                <Link to="/auth/registro">
+                    ¿No tienes cuenta? Crea una
+                </Link>
+            </nav>
+        </>
+    )
 }
